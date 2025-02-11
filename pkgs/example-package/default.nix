3477a18058b9fe729366bab732be23bdb9df4609
suimong <bennyyjx@gmail.com>
2025-02-11 13:36:48 +0800

    fix

--- pkgs/example-package/default.nix
+++ pkgs/example-package/default.nix
@@ -2,7 +2,7 @@
 
 stdenv.mkDerivation rec {
   name = "example-package-${version}";
-  pname = "example1",
+  pname = "example1";
   version = "1.0";
   src = ./.;
   buildPhase = "echo echo Hello World > example";
